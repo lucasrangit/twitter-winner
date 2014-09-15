747c65680850f1514f76ba0c0a13ca68b39187b5
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2014-09-15 06:49:45 -0700

    use new webapp2 framework and specify Python 2.7 explicitly.

--- twitter-winner/main.py
+++ twitter-winner/main.py
@@ -1,7 +1,45 @@
+# Twitter Winner
+# Based on Python CLI application by Alex Eames http://raspi.tv/?p=5281
+# http://www.billthelizard.com/2013/12/creating-twitter-bot-on-google-app.html
+
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
 
 import tweepy
+import random
+import secrets
+
+# Consumer keys and access tokens, used for OAuth
+consumer_key = secrets.consumer_key
+consumer_secret = secrets.consumer_secret
+access_token = secrets.access_token
+access_token_secret = secrets.access_token_secret
+
+def pick_winner():
+    # OAuth process, using the keys and tokens
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    
+    # Creation of the actual interface, using authentication
+    api = tweepy.API(auth)
+
+    print "Retweets: ([enter] to select, any key to see next retweet)"
+    winning_rewtweet = None
+    for retweet in tweepy.Cursor(api.retweets_of_me).items():
+        print "(%ix) %s" % (retweet.retweet_count, retweet.text)
+        if str(raw_input('? ')) == '':
+            winning_rewtweet = retweet
+            break
+            
+    if not winning_rewtweet:
+        print "You must select a retweet to pick a winner from"
+        return
+        
+    retweets = api.retweets(winning_rewtweet.id)
+    
+    random_number = random.randint(0, len(retweets)-1)
+    winner = retweets[random_number].user
+    print "The winner is @%s !" % (winner.screen_name)
+
 
 class MainPage(webapp.RequestHandler):
     
@@ -12,9 +50,3 @@ class MainPage(webapp.RequestHandler):
 
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
 
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
