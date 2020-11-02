b9a45f9698b7ad2b5b82f54f83c8bb7cc5dfe5cf
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2020-11-02 21:43:52 +0100

    remove unused code

--- twitter-winner/main.py
+++ twitter-winner/main.py
@@ -2,7 +2,7 @@
 # Based on Python CLI application by Alex Eames http://raspi.tv/?p=5281
 # http://www.billthelizard.com/2013/12/creating-twitter-bot-on-google-app.html
 
-from google.appengine.ext import webapp 
+from google.appengine.ext import webapp
 from webapp2 import Route
 
 import tweepy
@@ -27,45 +27,18 @@ app_config = {
   }
 }
 
-def pick_winner():
-    # OAuth process, using the keys and tokens
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    
-    # Creation of the actual interface, using authentication
-    api = tweepy.API(auth)
-
-    print "Retweets: ([enter] to select, any key to see next retweet)"
-    winning_rewtweet = None
-    for retweet in tweepy.Cursor(api.retweets_of_me).items():
-        print "(%ix) %s" % (retweet.retweet_count, retweet.text)
-        if str(raw_input('? ')) == '':
-            winning_rewtweet = retweet
-            break
-            
-    if not winning_rewtweet:
-        print "You must select a retweet to pick a winner from"
-        return
-        
-    retweets = api.retweets(winning_rewtweet.id)
-    
-    random_number = random.randint(0, len(retweets)-1)
-    winner = retweets[random_number].user
-    print "The winner is @%s !" % (winner.screen_name)
-
 routes = [
   Route('/', handler='handlers.RootHandler'),
   Route('/followers', handler='handlers.FollowersHandler'),
   Route('/retweets', handler='handlers.RetweetsHandler'),
   Route('/searches', handler='handlers.SearchesHandler'),
   Route('/profile', handler='handlers.ProfileHandler', name='profile'),
-  
+
   Route('/logout', handler='handlers.AuthHandler:logout', name='logout'),
-  Route('/auth/<provider>', 
+  Route('/auth/<provider>',
     handler='handlers.AuthHandler:_simple_auth', name='auth_login'),
-  Route('/auth/<provider>/callback', 
+  Route('/auth/<provider>/callback',
     handler='handlers.AuthHandler:_auth_callback', name='auth_callback'),
 ]
 
 application = webapp.WSGIApplication(routes, config=app_config, debug=True)
-
